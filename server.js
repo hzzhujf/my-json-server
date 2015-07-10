3fcef6007a771d6a1b9b87814400a5d95e813145
zhenkunou <ryan.ouzhenkun@gmail.com>
2015-07-10 11:40:13 +0800

    Mock server api logic

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ server.use(jsonServer.defaults)
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }))
 
+server.all('*', function (req, res, next) {
+  res.header('Access-Control-Expose-Headers', 'X-Pagination');
+  next();
+});
+
 server.get('/api/test', function (req, res) {
   res.send('hello world!')
 })
@@ -20,12 +25,13 @@ server.get('/api/test', function (req, res) {
 var mkMoney = function() {
   return Number(faker.finance.amount())
 }
-var mkTrade = function(name, status, amount, date) {
+var mkTrade = function(title, desc, fee, time) {
   return {
-    name     : name   || '纯色',
-    status   : status || 'order',
-    amount   : amount || mkMoney(),
-    create_at: date   || '2015年4月21日14：44',
+    _id: faker.finance.account(),
+    title: title,
+    description: desc,
+    fee: fee,
+    time: time,
   }
 }
 var mkMemberDB = function() {
@@ -34,78 +40,90 @@ var mkMemberDB = function() {
     password: '123123',
     remain: mkMoney(),
     isSetPassword: false,
+    memberGuide: 'http://dev.dudumeijia.com/member-guide',
     recharges: [
       {_id: faker.finance.account(), price: 5000, bonus: 600 },
       {_id: faker.finance.account(), price: 3000, bonus: 300 },
       {_id: faker.finance.account(), price: 1000, bonus: 0 },
     ],
     trades: [
-      mkTrade('充值', 'recharge'),
-      mkTrade('充值', 'recharge'),
-      mkTrade('充值', 'recharge'),
-      mkTrade('纯色', 'pay'),
-      mkTrade('纯色', 'refund'),
-      mkTrade('充值', 'recharge'),
-    ]
+      mkTrade('充值', '充值成功', 3000, '2015年4月1日'),
+      mkTrade('充值', '充值成功', 2000, '2015年4月2日'),
+      mkTrade('充值', '充值成功', 1000, '2015年4月3日'),
+      mkTrade('纯色', '支付成功', -168, '2015年4月3日'),
+      mkTrade('纯色', '支付成功', -199, '2015年4月4日'),
+      mkTrade('充值', '充值成功', 299,  '2015年4月7日'),
+    ].concat(_.times(100, function(n) {
+      return mkTrade('纯色', '支付成功', -199, '2015年4月4日');
+    })),
   }
 }
 var memberdb = mkMemberDB()
-server.get('/reset', function(req, res) {
-  memberdb = mkMemberDB()
+server.get('/debug', function(req, res) {
+  if (req.query.reset) {
+    memberdb = mkMemberDB()
+  }
+  res.send( memberdb )
 })
-server.get('/userAccounts/me', function(req, res) {
+server.get('/user/userAccounts/me', function(req, res) {
   res.send(
     _.pick(memberdb, 'remain', 'isSetPassword')
   )
 })
-server.get('/userAccounts/rechargeInfo', function(req, res) {
+server.get('/user/userAccounts/rechargeInfo', function(req, res) {
   res.send(
-    _.pick(memberdb, 'remain', 'isSetPassword', 'recharges')
+    _.pick(memberdb, 'remain', 'isSetPassword', 'recharges', 'memberGuide')
   )
 })
-server.post('/userAccounts/redeem', function(req, res) {
+server.post('/user/redeem', function(req, res) {
   if (req.body.password != '1234') {
-    return res.send(403, {message: 'verify_code_match'})
+    return res.send(403, {
+      message: 'verify_code_match',
+      errtext: '兑换码无效',
+    })
   }
   var amount = 100
   memberdb.trades.push(
-    mkTrade('兑换', 'recharge', amount, new Date())
+    mkTrade('充值', '充值成功', amount, new Date().toString())
   )
   memberdb.remain += amount
   res.send(
     _.pick(memberdb, 'remain', 'isSetPassword')
   )
 })
-server.post('/userAccounts/pay', function(req, res) {
+server.post('/user/userAccounts/pay', function(req, res) {
   console.log(req.body);
   var chargeInfo = _.find(memberdb.recharges, {_id: req.body.chargeId})
   var amount = chargeInfo.price + chargeInfo.bonus
   memberdb.trades.push(
-    mkTrade('充值', 'recharge', amount, new Date())
+    mkTrade('充值', '充值成功', amount, new Date().toString())
   )
   memberdb.remain += amount
   res.send(
     _.pick(memberdb, 'remain', 'isSetPassword')
   )
 })
-server.post('/userAccounts/setPassword', function(req, res) {
+server.post('/user/userAccounts/setPassword', function(req, res) {
   memberdb.isSetPassword = true
   memberdb.password = req.body.password
   res.send(
     _.pick(memberdb, 'isSetPassword')
   )
 })
-server.post('/userAccounts/checkPassword', function(req, res) {
+server.post('/user/userAccounts/checkPassword', function(req, res) {
   if (memberdb.password != req.body.password) {
-    return res.send(403, {message: 'password_not_match'})
+    return res.send({ result: false })
   }
   res.send(
-    {message: 'success'}
+    {result: true}
   )
 })
-server.post('/userAccounts/changePassword', function(req, res) {
+server.post('/user/userAccounts/changePassword', function(req, res) {
   if (memberdb.password != req.body.oldPassword) {
-    return res.send(403, {message: 'password_not_match'})
+    return res.send(403, {
+      message: 'password_not_match',
+      errtext: '输入密码错误，请重试',
+    })
   }
   memberdb.password = req.body.newPassword
   memberdb.isSetPassword = true
@@ -113,9 +131,12 @@ server.post('/userAccounts/changePassword', function(req, res) {
     {message: 'success'}
   )
 })
-server.post('/userAccounts/resetPassword', function(req, res) {
+server.post('/user/userAccounts/forgetPassword', function(req, res) {
   if (memberdb.verifyCode != req.body.verifyCode) {
-    return res.send(403, {message: 'verify_code_invalid'})
+    return res.send(403, {
+      message: 'verify_code_invalid',
+      errtext: '验证码错误',
+    })
   }
   memberdb.password = req.body.newPassword
   memberdb.isSetPassword = true
@@ -123,10 +144,21 @@ server.post('/userAccounts/resetPassword', function(req, res) {
     {message: 'success'}
   )
 })
-server.get('/userAccounts/trades', function(req, res) {
-  res.send(
-    _.pick(memberdb, 'trades')
-  )
+server.get('/user/tradeRecords/dudu', function(req, res) {
+  var page = req.query.page
+  var per_page = req.query.per_page
+  var start = (page - 1) * per_page
+  var end = page * per_page
+  var data = memberdb.trades.filter(function(item, i) {
+    return start <= i && i < end;
+  })
+  res.setHeader('X-Pagination', JSON.stringify({
+    page: parseInt(page),
+    per_page: parseInt(per_page),
+    page_num: Math.ceil(memberdb.trades.length / per_page),
+    total_num: memberdb.trades.length,
+  }))
+  res.send(data)
 })
 server.post('/user/verify/sms', function(req, res) {
   res.send(
